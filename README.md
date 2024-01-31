2baa542606f99449cb6d5199ebee7a7dd75cb851
fb0801 <fbhatti0801@gmail.com>
2024-01-31 17:00:48 +0000

    project setup

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # react-recipe-app
 React recipe app
+
+
+# Resources used:
+
+https://www.youtube.com/watch?v=tvfeBLMA_Q4
+
+
+original repo -> https://github.com/hamza-mirza/react-recipe-app
---
+++ recipe-app/src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h1>recipe Search</h1>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
