ebbbc91563944adf350e409cf0c4800d9c7fcfdb
fb0801 <fbhatti0801@gmail.com>
2024-02-01 16:39:05 +0000

    1

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -6,12 +6,19 @@ const API_KEY = ""
 
 
 class App extends Component {
+  state = {
+    recipes: []
+  }
 
   getRecipe = async(e) => {
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
     const api_call = await fetch(``)
+
+     const data = await api_call.json()
+     this.setState({recipes: data.recipes})
   }
+   
 
   render() {
     return (
@@ -20,6 +27,9 @@ class App extends Component {
           <h1 className="App-title">Recipe Search</h1>
         </header>
         <Form getRecipe={this.getRecipe} />
+        {this.state.recipes.map((recipe) => {
+            return <p>{recipe.recipe_id}</p>
+        } ) }
       </div>
     );
   }
