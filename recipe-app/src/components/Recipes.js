89704be850ff189c01f0c0c5f9f970ca313f01d2
fb0801 <fbhatti0801@gmail.com>
2024-02-01 16:51:03 +0000

    recipe component 2

--- recipe-app/src/components/Recipes.js
+++ recipe-app/src/components/Recipes.js
@@ -4,7 +4,14 @@ import React, { Component } from 'react';
 
 const Recipes = props => (
     <div>
-
+             {props.recipes.map((recipe) => {
+            return (
+              <div key={recipe.recipe_id}>
+                <img src={recipe.image_url} alt={recipe.title} />
+                <p>{recipe.title}</p>
+              </div>
+            )
+        } ) }
     </div>
 )
 export default Recipes
