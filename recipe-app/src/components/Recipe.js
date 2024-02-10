ad2947eccb74c611adb45066f0e8336a4202e6aa
fb0801 <fbhatti0801@gmail.com>
2024-02-10 18:13:36 +0000

    recipes displaying 3

--- recipe-app/src/components/Recipe.js
+++ recipe-app/src/components/Recipe.js
@@ -1,9 +1,30 @@
 import React from "react";
 
+const API_KEY = ""
+
+
 class Recipe extends React.Component {
+    state = {
+        activeRecipe : []
+    }
+    componentDidMount = () => {
+        const title = this.props.location.state.recipe;
+       
+        const req = await fetch(``)
+    
+         const res = await req.json()
+
+         this.setState({ activeRecipe: res.recipes[0] })
+         
+    }
     render() {
         return(
-            <div>Recipe Component!</div>
+            <div className="container">
+                <div className="active-recipe">
+                    <img src="" alt=""/>
+                </div>
+
+            </div>
         )
     }
 }
