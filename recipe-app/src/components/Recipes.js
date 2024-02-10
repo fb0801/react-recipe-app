5f72060f07cfa0ad63a00d06ebf2fc2ba5c04402
fb0801 <fbhatti0801@gmail.com>
2024-02-10 15:39:24 +0000

    styling the recipe 2

--- recipe-app/src/components/Recipes.js
+++ recipe-app/src/components/Recipes.js
@@ -7,10 +7,13 @@ const Recipes = props => (
       <div className='row'>
       {props.recipes.map((recipe) => {
             return (
-              <div key={recipe.recipe_id}>
-                <img src={recipe.image_url} alt={recipe.title} />
-                <p>{recipe.title}</p>
+             <div className="col-md-4">
+              <div className='recipe__box'> 
+              <img className="recipe__box-img" src={rec.img_url} alt={recipe.title} />
+              
               </div>
+             </div>
+
             )
         } ) }
       </div>
